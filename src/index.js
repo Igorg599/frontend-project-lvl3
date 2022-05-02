79af680050458cfe282b9c582cad3b97db90a688
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-05-02 12:32:53 +0500

    setup i18next

--- src/index.js
+++ src/index.js
@@ -2,31 +2,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import { object, string } from 'yup';
+import i18n from 'i18next';
 import watchedObject from './watchers.js';
+import resources from './locales/index.js';
 
-const form = document.querySelector('form');
+const app = async () => {
+  const defaultLanguage = 'ru';
 
-const userSchema = object({
-  website: string().url().nullable(),
-});
+  const i18nInstance = i18n.createInstance();
+  await i18nInstance.init({
+    lng: defaultLanguage,
+    debug: false,
+    resources,
+  });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  userSchema
-    .validate({
-      website: formData.get('url'),
-    })
-    .then((res) => {
-      if (watchedObject.streams.includes(res.website)) {
-        watchedObject.error = 'RSS уже существует';
-        return;
-      }
-      watchedObject.error = null;
-      watchedObject.streams = [...watchedObject.streams, res.website];
-      form.reset();
-    })
-    .catch(() => {
-      watchedObject.error = 'Ссылка должна быть валидным URL';
-    });
-});
+  const userSchema = object({
+    website: string().url().nullable(),
+  });
+
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    userSchema
+      .validate({
+        website: formData.get('url'),
+      })
+      .then((res) => {
+        if (watchedObject.streams.includes(res.website)) {
+          watchedObject.error = i18nInstance.t('errors.double');
+          return;
+        }
+        watchedObject.error = null;
+        watchedObject.streams = [...watchedObject.streams, res.website];
+        form.reset();
+      })
+      .catch(() => {
+        watchedObject.error = i18nInstance.t('errors.valid');
+      });
+  });
+};
+
+app();
---
+++ src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    errors: {
+      valid: 'Ссылка должна быть валидным URL',
+      double: 'RSS уже существует',
+      resource: 'Ресурс не содержит валидный RSS',
+    },
+  },
+};
