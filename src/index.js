17585aa4f90a57ed08d73d80623bf1d17bbff7d5
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-05-02 10:23:44 +0500

    treatment form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 /* eslint-env browser */
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import { object, string, number, date, InferType } from "yup"
+import onChange from "on-change"
 
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  console.log(e);
-});
+const form = document.querySelector("form")
+const input = document.querySelector(".form-control")
+
+let userSchema = object({
+  website: string().url().nullable(),
+})
+
+const initialState = {
+  streams: [],
+  error: null,
+}
+
+const watchedObject = onChange(
+  initialState,
+  function (path, value, previousValue, applyData) {
+    watchedObject.error
+      ? input.classList.add("is-invalid")
+      : input.classList.remove("is-invalid")
+  }
+)
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const formData = new FormData(e.target)
+  userSchema
+    .validate({
+      website: formData.get("url"),
+    })
+    .then((res) => {
+      if (watchedObject.streams.includes(res.website)) {
+        watchedObject.error = "error"
+        return
+      }
+      watchedObject.streams = [...watchedObject.streams, res.website]
+      watchedObject.error = null
+      form.reset()
+    })
+    .catch((err) => {
+      watchedObject.error = "error"
+    })
+})
