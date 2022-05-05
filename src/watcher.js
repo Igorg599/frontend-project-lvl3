3dafded0c999664fc0e09c658adb7ff6bc045bbb
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-05-05 10:52:27 +0500

    changePosts

--- src/watcher.js
+++ src/watcher.js
@@ -2,7 +2,7 @@ import onChange from 'on-change';
 
 const watch = (elements, initialState) => {
   const {
-    input, feedback, form, feedsWrapper,
+    input, feedback, form, feedsWrapper, postsWrapper,
   } = elements;
 
   const changeForm = (state) => {
@@ -52,6 +52,53 @@ const watch = (elements, initialState) => {
     }
   };
 
+  const changePosts = (state) => {
+    const { posts } = state;
+    if (posts.length) {
+      const container = document.createElement('div');
+      container.classList.add('card', 'border-0');
+      postsWrapper.appendChild(container);
+      const titleWrapper = document.createElement('div');
+      titleWrapper.classList.add('card-body');
+      container.appendChild(titleWrapper);
+      const title = document.createElement('h2');
+      title.classList.add('card-title', 'h4');
+      titleWrapper.appendChild(title);
+      title.textContent = 'Посты';
+      const ulPost = document.createElement('ul');
+      ulPost.classList.add('list-group', 'border-0', 'rounded-0');
+      container.appendChild(ulPost);
+      posts.forEach((post) => {
+        const li = document.createElement('li');
+        li.classList.add(
+          'list-group-item',
+          'd-flex',
+          'justify-content-between',
+          'align-items-start',
+          'border-0',
+          'border-end-0',
+        );
+        const link = document.createElement('a');
+        link.classList.add('fw-bold');
+        link.setAttribute('href', post.link);
+        link.setAttribute('data-id', post.id);
+        link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noopener noreferrer');
+        link.textContent = post.title;
+        li.appendChild(link);
+        const button = document.createElement('button');
+        button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+        button.setAttribute('type', 'button');
+        button.setAttribute('data-id', post.id);
+        button.setAttribute('data-bs-toggle', 'modal');
+        button.setAttribute('data-bs-target', '#modal');
+        button.textContent = 'Просмотр';
+        li.appendChild(button);
+        ulPost.appendChild(li);
+      });
+    }
+  };
+
   const watchedObject = onChange(initialState, (path) => {
     switch (path) {
       case 'form':
@@ -59,6 +106,9 @@ const watch = (elements, initialState) => {
         break;
       case 'feeds':
         changeFeeds(initialState);
+        break;
+      case 'posts':
+        changePosts(initialState);
         break;
       default:
         break;
