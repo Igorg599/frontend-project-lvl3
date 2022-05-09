e7fd889bd877be0094dd4dd181def4c21af03d4c
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-05-09 11:48:16 +0500

    edit structure app

--- src/app.js
+++ src/app.js
@@ -6,81 +6,6 @@ import watch from './watcher.js';
 import resources from './locales/index.js';
 import parser from './parser.js';
 
-const fetchInterval = 5000;
-
-const proxyLink = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
-
-const typeError = (err, i18nInstance) => {
-  if (err.message === 'Network Error') {
-    return i18nInstance.t('errors.network');
-  }
-  if (err.isResource) {
-    return i18nInstance.t('errors.resource');
-  }
-  return i18nInstance.t('errors.unknown');
-};
-
-const getNewPosts = (state) => {
-  const promisesFeeds = state.feeds.map((feed) => axios
-    .get(proxyLink + feed.url)
-    .then((response) => {
-      const data = parser(response.data.contents);
-
-      const newPosts = data.items.map((item) => ({
-        ...item,
-        feedId: feed.id,
-      }));
-      const oldPosts = state.posts.filter((post) => post.feedId === feed.id);
-
-      const resultPosts = _.differenceWith(
-        newPosts,
-        oldPosts,
-        (paramOne, paramTwo) => paramOne.title === paramTwo.title,
-      ).map((post) => ({ ...post, id: _.uniqueId() }));
-
-      state.posts.unshift(...resultPosts);
-    })
-    .catch((err) => console.log(err)));
-
-  Promise.all(promisesFeeds).finally(() => {
-    setTimeout(() => getNewPosts(state), fetchInterval);
-  });
-};
-
-const processSSr = (url, state, i18nInstance) => {
-  state.load = 'loading';
-  axios
-    .get(proxyLink + url)
-    .then((response) => {
-      const dataParse = parser(response.data.contents);
-      const feed = {
-        url,
-        id: _.uniqueId(),
-        title: dataParse.title,
-        descr: dataParse.descr,
-      };
-      const posts = dataParse.items.map((item) => ({
-        ...item,
-        feedId: feed.id,
-        id: _.uniqueId(),
-      }));
-      state.feeds.unshift(feed);
-      state.posts.unshift(...posts);
-      state.load = 'ok';
-      state.form = {
-        error: null,
-        valid: true,
-      };
-    })
-    .catch((e) => {
-      state.form = {
-        error: typeError(e, i18nInstance),
-        valid: false,
-      };
-      state.load = 'failure';
-    });
-};
-
 const app = async () => {
   const defaultLanguage = 'ru';
 
@@ -117,6 +42,81 @@ const app = async () => {
     load: 'ok',
   };
 
+  const fetchInterval = 5000;
+
+  const proxyLink = 'https://allorigins.hexlet.app/get?disableCache=true&url=';
+
+  const typeError = (err) => {
+    if (err.message === 'Network Error') {
+      return i18nInstance.t('errors.network');
+    }
+    if (err.isResource) {
+      return i18nInstance.t('errors.resource');
+    }
+    return i18nInstance.t('errors.unknown');
+  };
+
+  const getNewPosts = (state) => {
+    const promisesFeeds = state.feeds.map((feed) => axios
+      .get(proxyLink + feed.url)
+      .then((response) => {
+        const data = parser(response.data.contents);
+
+        const newPosts = data.items.map((item) => ({
+          ...item,
+          feedId: feed.id,
+        }));
+        const oldPosts = state.posts.filter((post) => post.feedId === feed.id);
+
+        const resultPosts = _.differenceWith(
+          newPosts,
+          oldPosts,
+          (paramOne, paramTwo) => paramOne.title === paramTwo.title,
+        ).map((post) => ({ ...post, id: _.uniqueId() }));
+
+        state.posts.unshift(...resultPosts);
+      })
+      .catch((err) => console.log(err)));
+
+    Promise.all(promisesFeeds).finally(() => {
+      setTimeout(() => getNewPosts(state), fetchInterval);
+    });
+  };
+
+  const processSSr = (url, state) => {
+    state.load = 'loading';
+    axios
+      .get(proxyLink + url)
+      .then((response) => {
+        const dataParse = parser(response.data.contents);
+        const feed = {
+          url,
+          id: _.uniqueId(),
+          title: dataParse.title,
+          descr: dataParse.descr,
+        };
+        const posts = dataParse.items.map((item) => ({
+          ...item,
+          feedId: feed.id,
+          id: _.uniqueId(),
+        }));
+        state.feeds.unshift(feed);
+        state.posts.unshift(...posts);
+        state.load = 'ok';
+        state.form = {
+          error: null,
+          valid: true,
+        };
+      })
+      .catch((e) => {
+        state.form = {
+          error: typeError(e),
+          valid: false,
+        };
+        state.load = 'failure';
+      });
+  };
+
   const validateURL = (url, state) => {
     const links = state.feeds.map((feed) => feed.url);
     if (links.includes(url)) {
@@ -144,7 +144,7 @@ const app = async () => {
             valid: false,
           };
         } else {
-          processSSr(url, watchState, i18nInstance);
+          processSSr(url, watchState);
         }
       })
       .catch((err) => {
